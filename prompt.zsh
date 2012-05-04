e5a53807eaa84f7663ef86f7286b52a105d49b14
Arkadiusz Hiler <arkadiusz@hiler.pl>
2012-05-04 12:40:34 +0200

    new prompt

--- prompt.zsh
+++ prompt.zsh
@@ -21,7 +21,7 @@ setopt prompt_subst
 #PROMPT="${at_bold}${fg_dgray}[ ${fg_red}%n ${fg_white}%~ ${fg_dgray}] ${fg_white}\$${at_normal} "
 
 #prompt with hostname (old, simple)
-PROMPT="${at_bold}${fg_dgray}[ ${fg_red}%n${fg_white}@%m %~\${vcs_info_msg_0_} ${fg_dgray}] ${fg_white}\$${at_normal} "
+#PROMPT="${at_bold}${fg_dgray}[ ${fg_red}%n${fg_white}@%m %~\${vcs_info_msg_0_} ${fg_dgray}] ${fg_white}\$${at_normal} "
 
 #new testing prompt version
-#PROMPT="${at_bold}%m ${fg_red}%n ${fg_blue}%c\${vcs_info_msg_0_} %(?/${at_normal}/${fg_red})%%${at_normal} "
+PROMPT="${at_bold}%m ${fg_red}%n ${fg_blue}%c\${vcs_info_msg_0_} %(?/${at_normal}/${fg_red})%%${at_normal} "
