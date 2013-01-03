81b1cab2700d124865b2d2e144eda5062285fd60
Arkadiusz Hiler <arkadiusz@hiler.pl>
2013-01-03 20:56:46 +0100

    add colors to less/mans

--- config.zsh
+++ config.zsh
@@ -48,11 +48,20 @@ fi
 PATH="$PATH:$HOME/.bin"
 
 
-#EXPORTS
-export PAGER='less'
+#EDITORS
 export EDITOR='vim'
 export VISUAL='vim'
+
+#PAGER
+export PAGER='less'
 export LESS=-ex3MRXSF
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;31m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;47;30m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;32m'
 
 #loading functions
 autoload -Uz zmv
