a2f083d6ebe85487380878229b5633df99996703
Arkadiusz Hiler <arkadiusz@hiler.pl>
2016-07-25 23:31:54 +0200

    change prompt colors to numeric values

--- prompt.zsh
+++ prompt.zsh
@@ -21,8 +21,8 @@ setopt prompt_subst
 
 PROMPT="${at_bold}%m ${fg_red}%n ${fg_blue}%c\${vcs_info_msg_0_} %(?/${at_normal}/${fg_red})%%${at_normal} "
 
-INSERT_PROMPT="gray"
-COMMAND_PROMPT="red"
+INSERT_PROMPT="256"
+COMMAND_PROMPT="1"
 
 set_prompt_color() {
     if [[ $TERM = "linux" ]]; then
