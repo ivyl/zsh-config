78f841f58b67aaa85b6d1c1107e6a2961de583ed
Arkadiusz Hiler <arek@hiler.eu>
2020-08-10 09:54:35 +0300

    up: Allow to upload files

--- functions.zsh
+++ functions.zsh
@@ -28,9 +28,28 @@ mkday()
 }
 
 up() {
-    local name=$(uuidgen | cut -d- -f1)
-    ssh verne cat \> hiler.eu/p/$name.txt
-    echo https://hiler.eu/p/$name.txt
+    local name=$(uuidgen | cut -d- -f5)
+    local ext="txt"
+    local place="p"
+
+    if [ -n "$1" ]; then
+        ext="${1##*.}"
+        local ext_length="$(expr length "$ext")"
+        if [ "$ext_length" -ne 3 -a "$ext_length" -ne 4 ]; then
+            echo "$ext $ext_length"
+            echo "extensions should be 3 or 4!"
+            return 1
+        fi
+        place="f"
+    fi
+
+    if [ -n "$1" ]; then
+        scp "$1" verne:hiler.eu/$place/$name.$ext
+    else
+        ssh verne cat \> hiler.eu/$place/$name.$ext
+    fi
+
+    echo https://hiler.eu/$place/$name.$ext
 }
 
 
